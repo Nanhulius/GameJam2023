bee1948a5285013f2e8e41026a5783dfe8c43067
GameJam23 <github.fujikura@simplelogin.com>
2023-02-05 11:39:13 +0200

    Updated Level Exits

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -7,17 +7,6 @@ public class PlayerData : MonoBehaviour
 {
     [SerializeField] int maxHealth = 1;
     [SerializeField] int health = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void GameOver() {
         Debug.Log("Game Over!");
