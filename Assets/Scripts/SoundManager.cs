f454bc35ae927e311fc8816429ca01813451aaa8
Nanhulius <hrosirvio@gmail.com>
2023-02-05 11:57:02 +0200

    SFX has a timer now

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,14 +7,34 @@ public class SoundManager : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip[] jumpSFX;
 
+    public float waitForSFX = 0f;
+
+    public bool sfxTimer = false;
+
     private void Awake()
     {
-        audioSource = GetComponent<AudioSource>(); 
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     public void PlayJumpingAudio()
     {
-        audioSource.clip = jumpSFX[Random.Range(0, jumpSFX.Length)];
-        audioSource.Play();
+        if (!sfxTimer)
+        {
+            audioSource.clip = jumpSFX[Random.Range(0, jumpSFX.Length)];
+            audioSource.Play();
+            waitForSFX = Random.Range(5.5f, 10.0f);
+            StartCoroutine(WaitForJumpSound(waitForSFX));
+        }
+        else
+            return;      
+    }
+
+    private IEnumerator WaitForJumpSound(float waitForSFX)
+    {
+        sfxTimer = true; 
+        yield return new WaitForSeconds(waitForSFX);
+        sfxTimer = false;
+        StopCoroutine(WaitForJumpSound(waitForSFX));
     }
 }
